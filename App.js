15523cfee97d417f12fed1ea310f5bd12e6318ce
nikolay <kolja-savelev@mail.ru>
2019-03-14 20:03:21 +0300

    Add sessions

--- App.js
+++ App.js
@@ -8,12 +8,12 @@ const dateTime = require('node-datetime');
 const asr = require('./asr');
 const request = require('request-promise')
 
-
+const currentSessionFile = path.join(__dirname, '.vksession');
 
 easyvk({
-    password: "22808250Xrazik",
-    username: "79776625383",
-    reauth: true
+    reauth: false,
+    session_file: currentSessionFile,
+    save_session: false
   }).then(async (vk) => {
       
     console.log("Vk logging is up and writing in logs.txt");
@@ -93,59 +93,6 @@ easyvk({
     });
     
     
-
-
-
-    // const session = vk.session;
-
-    // const lpSettings = {
-    //     forGetLongPollServer: {
-    //         lp_version: "3", //Изменяем версию LongPoll, в EasyVK используется версия 2
-    //         need_pts: "1"
-    //     },
-    //     forLongPollServer: {
-    //         wait: "15" //Ждем ответа 15 секунд
-    //     }
-    // }
-    
-    // function longPollDebugger({type, data}) {
-    //     // Конвертируем JSON в объект
-    //     if (type == "pollResponse"){
-    //         let json = JSON.parse(data);
-
-    //         console.log(json.updates[0]);
-
-    //         if (typeof json.updates == "undefined"){
-    //             return;
-    //         }
-
-    //         if (!(0 in json.updates)){
-    //             return;
-    //         }
-
-
-    //         let resCode = json.updates[0][0];
-
-    //         if(resCode == 4){
-    //             let dt = dateTime.create();
-    //             let date = dt.format('Y-m-d H:M:S');
-
-    //             let text = `\n${date}\n---------\n${json.updates}\n----------`;
-            
-    //             fs.appendFile("logs.txt", text, function(error){
-    //                 if(error) throw error; 
-    //             });
-    //         }
-    //     }
-    // }
-      
-    //   vk.longpoll.connect()
-    //   .then(({connection: lpcon}) => {
-      
-    //     lpcon.debug(longPollDebugger);
-
-        
-    //   })
   
 }).catch(error => {
     console.log(error);
