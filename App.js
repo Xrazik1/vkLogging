60af4a8ec918a630ba57e234f4ab33150997ff0e
nikolay <kolja-savelev@mail.ru>
2019-03-16 21:29:17 +0300

    Google speech-kit added

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 const easyvk = require('easyvk');
-const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const http = require("http");
 const fs = require("fs");
 const dateTime = require('node-datetime');
 const asr = require('./asr');
-const request = require('request-promise')
+const request = require("request");
 
-const currentSessionFile = path.join(__dirname, '.vksession');
+
+let handleAudioPhrase = (audioPhrase, fullMessage, vk) => {
+  let dt = dateTime.create();
+  let date = dt.format('Y-m-d H:M:S');
+
+  let text = `\n${date}\n---------\nuserId: ${ fullMessage.user_id }, audioMessage: ${ audioPhrase }\n----------`;
+
+  fs.appendFile("logs.txt", text, function(error){
+      if(error) throw error; 
+  });
+
+  // if(fullMessage.chat_id != 17){
+    vk.call('messages.send', {
+      user_id: fullMessage.user_id,
+      message: audioPhrase
+    })
+  // }
+}
+
+
+const currentSessionFile = path.join(__dirname, '.vksession')
 
 easyvk({
-    reauth: true,
+    reauth: false,
     password: "22808250Xrazik",
     username: "79776625383",
     session_file: currentSessionFile,
     save_session: true
   }).then(async (vk) => {
-      
+    console.log("Vk logging is up and writing in logs.txt");
 
     async function getMessage (msgArray = []) {
       const MESSAGE_ID__INDEX = 1;
@@ -26,12 +43,9 @@ easyvk({
         message_ids: msgArray[MESSAGE_ID__INDEX]
       })
     }
-    
-
+  
 
     let { connection } = await (vk.longpoll.connect());
-
-    console.log("Vk logging is up and writing in logs.txt");
 
     connection.on("message", async (msg) => {
   
@@ -44,7 +58,6 @@ easyvk({
       if( fullMessage.body != "" ){
         let dt = dateTime.create();
         let date = dt.format('Y-m-d H:M:S');
-
         let text = `\n${date}\n---------\nuserId: ${ fullMessage.user_id }, message: ${ fullMessage.body }\n----------`;
     
         fs.appendFile("logs.txt", text, function(error){
@@ -61,48 +74,23 @@ easyvk({
 
       const uri = fullMessage["attachments"][0]["doc"]["preview"]["audio_msg"]["link_mp3"];
 
-      const audio = await request.get({ uri, encoding: null });
-      let audioPhrase;
-      
-      try {
-        audioPhrase = await asr(audio)
-      }
-      catch (e) {
-        console.error('asr error', e)
-        throw ('сбой в распознавании речи');
-      }
 
-      if(audioPhrase){
-        let dt = dateTime.create();
-        let date = dt.format('Y-m-d H:M:S');
+      request.get( { uri, encoding: null } ).on('response', async (response) => {
+        response.pipe(fs.createWriteStream("audio.mp3"));
 
-        let text = `\n${date}\n---------\nuserId: ${ fullMessage.user_id }, audioMessage: ${ audioPhrase }\n----------`;
-    
-        fs.appendFile("logs.txt", text, function(error){
-            if(error) throw error; 
-        });
+        let audioPhrase = await asr();
 
-        if(fullMessage.chat_id != 17){
-          vk.call('messages.send', {
-            user_id: fullMessage.user_id,
-            message: audioPhrase
-          })
-        }
+        handleAudioPhrase(audioPhrase, fullMessage, vk);
+      });
 
-
-      }
       
   
     });
     
     
-
   
 }).catch(error => {
-    console.log(error);
+    console.log(error)
 });
 
-
-
 process.on('unhandledRejection', console.error)
-
--- asr.js
+++ asr.js
@@ -1,20 +1,45 @@
-const request = require('request-promise');
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+const linear16 = require('linear16');
+ 
 
-const uri = `https://api.wit.ai/speech`
-const apikey = 'V4ZJUJ3BIOWVSKLQQUADGPJT33THHIU3' // получаем ключ доступа на wit.ai
+module.exports = async () => {
 
-module.exports = async (body) => {
-  // отправляем post запрос с буфером аудио
-  const response = await request.post({
-    uri,
-    headers: {
-      'Accept': 'audio/x-mpeg-3',
-      'Authorization': `Bearer ` + apikey,
-      'Content-Type': 'audio/mpeg3',
-      'Transfer-Encoding': 'chunked'
-    },
-    body
-  })
-  // парсим ответ и отдаем результат расшифровки
-  return JSON.parse(response)._text
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The name of the audio file to transcribe
+  const fileName = './audio.wav';
+
+
+  await linear16('./audio.mp3', './audio.wav');
+
+
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'ru-RU',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+
+  return transcription;
 }
+
+process.on('unhandledRejection', console.error)
